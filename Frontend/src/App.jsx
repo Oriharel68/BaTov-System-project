53ff132821fd1646856ebf1681dc8a39cbc8cbaf
Oriharel68 <133902451+Oriharel68@users.noreply.github.com>
2023-06-10 20:34:00 +0300

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Main from './Main page/Main'
+import RouterServer from './Router/RouterServer'
+
+function App() {
+  return (
+    <div>
+      <RouterServer/>
+      {/* <Main/> */}
+      {/* need to conatin the main router component that connect all the components */}
+    </div>
+  )
+}
+
+export default App
