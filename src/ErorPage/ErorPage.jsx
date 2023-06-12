f23ad4160d9cd0b20b68a8c932591f2b3d5fe0bf
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-06-12 16:17:30 +0300

    Add files via upload

---
+++ src/ErorPage/ErorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ErorPage() {
+  return (
+    <div>ErorPage 404!</div>
+  )
+}
+
+export default ErorPage
---
+++ src/nav/NavBar.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+// function NavBar() {
+//   return (
+//     <div className='navBar-Main'>
+//         <h1>Copmpany Logo</h1>
+//         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo et molestias corporis totam facilis voluptatibus dolor tempora velit accusantium facere, culpa blanditiis quae in eius est maxime nisi laudantium nulla?</p>
+//     </div>
+//   )
+// }
+
+// export default NavBar
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+    <div className='navBar-Main'>
+       <h1>logo</h1>
+     </div>
+    </div>
+  )
+}
+
+export default NavBar
