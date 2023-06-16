b537083c85d1de106d7f5ff72028dd1e1d993ee9
Oriharel68 <oriharel68@gmail.com>
2023-06-16 20:39:06 +0300

    Fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // import Main from './Main page/Main'
 import RouterServer from './Router/RouterServer'
+import 'normalize.css';
 
 function App() {
   return (
     <div>
       <RouterServer/>
       {/* <Main/> */}
-      {/* need to conatin the main router component that connect all the cדגכדomponents */}
+      {/* need to conatin the main router component that connect all the components */}
     </div>
   )
 }
--- src/ErorPage/ErorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ErorPage() {
-  return (
-    <div>ErorPage 404!</div>
-  )
-}
-
-export default ErorPage
