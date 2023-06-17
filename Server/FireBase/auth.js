d4321c4de69557a076c44fd0c9eb1ac5146db38e
Oriharel68 <oriharel68@gmail.com>
2023-06-17 22:30:26 +0300

    remove the auth

--- Server/FireBase/auth.js
+++ Server/FireBase/auth.js
@@ -1,19 +1,28 @@
 // Import the functions you need from the SDKs you need
 const initializeApp = require('firebase/app').initializeApp;
+require("dotenv").config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+const apiKey = process.env.apiKey;
+const authDomain = process.env.authDomain;
+const projectId = process.env.projectId;
+const storageBucket = process.env.storageBucket;
+const messagingSenderId = process.env.messagingSenderId;
+const appId = process.env.appId;
+const measurementId = process.env.measurementId;
 
+console.log(apiKey);
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVhBE1d4rIx-DpNOvVZtXYvFzFUEgkbj0",
-  authDomain: "intproject-652bb.firebaseapp.com",
-  projectId: "intproject-652bb",
-  storageBucket: "intproject-652bb.appspot.com",
-  messagingSenderId: "4884691304",
-  appId: "1:4884691304:web:50b360e69ac3b15444fedd",
-  measurementId: "G-L4HESCGGGK"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 // Initialize Firebase
