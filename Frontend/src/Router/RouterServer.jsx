4ef4181c3dc080212c2d5ff4a8663da672779fd1
Oriharel68 <oriharel68@gmail.com>
2023-07-05 18:56:39 +0300

    rempve

--- Frontend/src/Router/RouterServer.jsx
+++ Frontend/src/Router/RouterServer.jsx
@@ -8,7 +8,6 @@ import RegestrationPage from "../ClientPage/RegestrationPage";
 import AcsessPage from "../ClientPage/AcsessPage";
 import OrderMain from "../ClientPage/OrderMain";
 import NewOrder from "../ClientPage/NewOrder";
-import Calender from "../ClientPage/Calender/Calender";
 import CompanyAccsess from "../CompanyPage/CompanyAccsess";
 import CompantMainPage from "../CompanyPage/CompantMainPage";
 import AddworkerCompany from "../CompanyPage/subTitlelsComponents/AddworkerCompany";
@@ -34,7 +33,7 @@ function RouterServer() {
           {/* orders */}
           <Route path="/order/main" element={<OrderMain/>} />
           <Route path="/order/newOrder" element={<NewOrder/>} />
-          <Route path="/order/Calender" element={<Calender/>} />
+          
           {/*company side  */}
           <Route path="/company/access" element={<CompanyAccsess/>} />
           <Route path="/company/mainpage" element={<CompantMainPage/>} />
