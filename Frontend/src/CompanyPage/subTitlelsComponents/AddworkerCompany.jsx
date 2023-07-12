deffbd529390df4c1b146a45c76349eb65a8bba9
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-07-12 16:16:55 +0300

    update for addWorker compoent

--- Frontend/src/CompanyPage/subTitlelsComponents/AddworkerCompany.jsx
+++ Frontend/src/CompanyPage/subTitlelsComponents/AddworkerCompany.jsx
@@ -4,10 +4,33 @@ import CombaibnedNavCompany from '../../nav/CombaibnedNavCompany'
 function AddworkerCompany() {
   return (
     <div>
+      <div className="navCompany-container">
+
       <CombaibnedNavCompany/>
+      </div>
+      
+      <div className="mainAddWorker-Page">
+        <div className="main-container">
+          <h3>הוספת עובד</h3>
+          <form action="">
+            
+        <input type="text" id="W2" name="" placeholder='סוג איש מקצוע'/>
+        <input type="text" id="W2" name="" placeholder='שם עובד'/>
+        <input type="text" id="W3" name="" placeholder='מחיר/עלות בדיקה'/>
+         
+          <button type="button">
+            הוספה
+          </button>
+          </form>
+
+            
+            </div>
+      </div>
 
     </div>
   )
 }
-
+// input[type=text]:focus {
+  // background-color: lightblue;
+// }
 export default AddworkerCompany
