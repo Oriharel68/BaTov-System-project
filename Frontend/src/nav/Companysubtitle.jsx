5803c638046539b4153287dd64f2fe62e9587a82
Oriharel68 <oriharel68@gmail.com>
2023-07-25 20:52:24 +0300

    Update Companysubtitle.jsx

--- Frontend/src/nav/Companysubtitle.jsx
+++ Frontend/src/nav/Companysubtitle.jsx
@@ -1,45 +1,21 @@
-import React, { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from 'react'
+import { NavLink, useParams } from 'react-router-dom'
 
 function Companysubtitle() {
-  const [activeLink, setActiveLink] = useState("");
 
-  const Tchange = (link) => {
-    setActiveLink(link);
-  };
-
+ const path = window.location.pathname.replace('/company/','');
   return (
-    <div className="companySubTitle-conatiner">
-      <ul>
-        <Link to={"/company/Orders"} 
-   className={`T${activeLink === '/company/Orders' ? 'main' : ''}`}        
-   onClick={() => Tchange('/company/Orders')}
+    <div className='companySubTitle-conatiner'>
+        <ul >
+        <NavLink to={'/company/Orders'} className={`T ${path ==='Orders' ?'main':'' }`} >  <li>הזמנות</li> </NavLink> 
+            <NavLink to={'/company/Statistics'}className={`T ${path ==='Statistics' ?'main':'' }`} > <li>סטטיסטיקה</li> </NavLink> 
+           <NavLink to={'/company/Calender'}className={`T ${path === 'Calender' ?'main':'' }`}>  <li>יומן עבודה</li> </NavLink> 
+           <NavLink to={'/company/AddWorker'}className={`T ${path === 'AddWorker' ?'main':'' }`} ><li>הוספת עובד</li> </NavLink> 
+           <NavLink to={'/company/mainpage'}className={`T ${path === 'mainpage' ?'main':'' }  `} ><li >ראשי</li> </NavLink> 
+        </ul>
 
->
-          <li>הזמנות</li>
-        </Link>
-        <Link to={"/company/Statistics"} 
-           className={activeLink === '/company/Statistics' ? 'main' : ''}
-           onClick={() => Tchange('/company/Statistics')}
-
-        
-        >
-          {" "}
-          <li>סטטיסטיקה</li>{" "}
-        </Link>
-        <Link to={"/company/Calender"} className="T">
-          {" "}
-          <li>יומן עבודה</li>{" "}
-        </Link>
-        <Link to={"/company/AddWorker"} className="T">
-          <li>הוספת עובד</li>{" "}
-        </Link>
-        <Link to={"/company/mainpage"} className="main">
-          <li>ראשי</li>{" "}
-        </Link>
-      </ul>
     </div>
-  );
+  )
 }
 
-export default Companysubtitle;
+export default Companysubtitle
