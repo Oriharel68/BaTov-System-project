4cb52cab8bd45e95c127ea11c5b4f89206c16f23
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-07-25 20:41:25 +0300

    XXX

--- Frontend/src/nav/Companysubtitle.jsx
+++ Frontend/src/nav/Companysubtitle.jsx
@@ -1,21 +1,45 @@
-import React, { useRef, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Companysubtitle() {
+  const [activeLink, setActiveLink] = useState("");
 
+  const Tchange = (link) => {
+    setActiveLink(link);
+  };
 
   return (
-    <div className='companySubTitle-conatiner'>
-        <ul >
-        <NavLink to={'/company/Orders'} className='T' >  <li>הזמנות</li> </NavLink> 
-            <NavLink to={'/company/Statistics'}className='T' > <li>סטטיסטיקה</li> </NavLink> 
-           <NavLink to={'/company/Calender'}className='T'>  <li>יומן עבודה</li> </NavLink> 
-           <NavLink to={'/company/AddWorker'}className='T' ><li>הוספת עובד</li> </NavLink> 
-           <NavLink to={'/company/mainpage'}className='T' ><li >ראשי</li> </NavLink> 
-        </ul>
+    <div className="companySubTitle-conatiner">
+      <ul>
+        <Link to={"/company/Orders"} 
+   className={`T${activeLink === '/company/Orders' ? 'main' : ''}`}        
+   onClick={() => Tchange('/company/Orders')}
 
+>
+          <li>הזמנות</li>
+        </Link>
+        <Link to={"/company/Statistics"} 
+           className={activeLink === '/company/Statistics' ? 'main' : ''}
+           onClick={() => Tchange('/company/Statistics')}
+
+        
+        >
+          {" "}
+          <li>סטטיסטיקה</li>{" "}
+        </Link>
+        <Link to={"/company/Calender"} className="T">
+          {" "}
+          <li>יומן עבודה</li>{" "}
+        </Link>
+        <Link to={"/company/AddWorker"} className="T">
+          <li>הוספת עובד</li>{" "}
+        </Link>
+        <Link to={"/company/mainpage"} className="main">
+          <li>ראשי</li>{" "}
+        </Link>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Companysubtitle
+export default Companysubtitle;
