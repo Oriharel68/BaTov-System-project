46d4d0feed86e079e2334ea8adda2cb16c8d5036
Oriharel68 <oriharel68@gmail.com>
2023-07-26 18:34:36 +0300

    responsive dropdown list

--- Frontend/src/nav/Companysubtitle.jsx
+++ Frontend/src/nav/Companysubtitle.jsx
@@ -1,21 +1,116 @@
-import React, { useRef, useState } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaEllipsisV } from 'react-icons/fa';
 
 function Companysubtitle() {
+  const path = window.location.pathname.replace('/company', '');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
- const path = window.location.pathname.replace('/company/','');
+  // Update windowWidth state when the window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // Determine whether to show the dropdown based on windowWidth
+  const showDropdown = windowWidth <= 767;
+
   return (
     <div className='companySubTitle-conatiner'>
-        <ul >
-        <NavLink to={'/company/Orders'} className={`T ${path ==='Orders' ?'mainO':'' }`} >  <li>הזמנות</li> </NavLink> 
-            <NavLink to={'/company/Statistics'}className={`T ${path ==='Statistics' ?'main':'' }`} > <li>סטטיסטיקה</li> </NavLink> 
-           <NavLink to={'/company/Calender'}className={`T ${path === 'Calender' ?'main':'' }`}>  <li>יומן עבודה</li> </NavLink> 
-           <NavLink to={'/company/AddWorker'}className={`T ${path === 'AddWorker' ?'main':'' }`} ><li>הוספת עובד</li> </NavLink> 
-           <NavLink to={'/company/mainpage'}className="mainP"><li >ראשי</li> </NavLink> 
+      {/* Show the regular ul for larger screens */}
+      {!showDropdown && (
+        <ul className='desktop-list'>
+         
+          <li>
+            <NavLink to='/company/Orders' className={`T ${path === '/Orders' ? 'mainO' : ''}`}>
+              הזמנות
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/company/Statistics'
+              className={`T ${path === '/Statistics' ? 'main' : ''}`}
+            >
+              סטטיסטיקה
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/company/Calender' className={`T ${path === '/Calender' ? 'main' : ''}`}>
+              יומן עבודה
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/company/AddWorker' className={`T ${path === '/AddWorker' ? 'main' : ''}`}>
+              הוספת עובד
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/company/mainpage' className='mainP'>
+              ראשי
+            </NavLink>
+          </li>
         </ul>
+      )}
 
+      {/* Show the עוד button for smaller screens */}
+      {showDropdown && (
+        <>
+          <div
+            className={`more-button ${dropdownVisible ? 'active' : ''}`}
+            onClick={() => setDropdownVisible(!dropdownVisible)}
+          >
+            <FaEllipsisV />
+          </div>
+          {dropdownVisible && (
+            <div className='dropdown-list-wrapper'>
+              <ul className='dropdown-list'>
+                <li>
+                  <NavLink to='/company/mainpage' className='mainP'>
+                    ראשי
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/company/Orders' className={`T ${path === '/Orders' ? 'mainO' : ''}`}>
+                    הזמנות
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to='/company/Statistics'
+                    className={`T ${path === '/Statistics' ? 'main' : ''}`}
+                  >
+                    סטטיסטיקה
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to='/company/Calender'
+                    className={`T ${path === '/Calender' ? 'main' : ''}`}
+                  >
+                    יומן עבודה
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to='/company/AddWorker'
+                    className={`T ${path === '/AddWorker' ? 'main' : ''}`}
+                  >
+                    הוספת עובד
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Companysubtitle
+export default Companysubtitle;
