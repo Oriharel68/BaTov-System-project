5d0ac7abcc5e6dc2605425a1cdda70bfdfa03d4e
Oriharel68 <oriharel68@gmail.com>
2023-08-03 21:02:29 +0300

    delete unneccesry files

--- Frontend/src/MiddlewareAuth/MiddlewareAuth.jsx
+++ Frontend/src/MiddlewareAuth/MiddlewareAuth.jsx
@@ -3,13 +3,11 @@ import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function MiddlewareAuth({children}) {
-    const AuthEntry = []
     const Auth = getAuth();
     const navigate = useNavigate();
     const location = useLocation().pathname;
     useEffect(()=>{
-        console.log(location);
-        if((location.indexOf("/order")!==-1 || (location.indexOf("/company") !== -1 && (location !== '/company/access'))) && Auth.currentUser===null){
+        if((location.indexOf("/order") !== -1 || (location.indexOf("/company") !== -1 && (location !== '/company/access'))) && Auth.currentUser===null){
             navigate('/');
         }
     },[location]);
