0f5ef85248253ee96c8f86569c0aaf4c18ac9e16
Oriharel68 <oriharel68@gmail.com>
2023-08-07 22:47:10 +0300

    adding the icons for removeOrder

--- Frontend/src/ClientPage/ExistingOrderList.jsx
+++ Frontend/src/ClientPage/ExistingOrderList.jsx
@@ -3,6 +3,8 @@ import { setDate } from '../Helpjs/help';
 import Modal from 'react-modal';
 import RemoveStyle from './ModalStyle/RemoveOrderStyle';
 import axios from 'axios';
+import {MdRemoveCircleOutline} from 'react-icons/md'
+import {BiExit} from 'react-icons/bi'
 function ExistingOrderList({order,setChanged,Changed}) {
 
   const [showModal,setShowModal] = useState(false);
@@ -47,15 +49,23 @@ function ExistingOrderList({order,setChanged,Changed}) {
           <h3>מקצוע:{order.TypeOfService}</h3>
           <h3>שם עובד : {order.WorkerName}</h3>
           <h3>תאריך : {setDate(order.DateTime)}</h3>
-          <div>
-          <button onClick={handleClick}>יציאה</button>
-      <button onClick={handleRemove}>מחיקה</button>
+          <div style={{display:'flex',justifyContent:'space-between',maxWidth:'100%',width:'80%'}}>
+            <div style={{cursor:'pointer',display:'flex',flexDirection:'column'}}>
+            <BiExit style={{width:'3em',height:'3em'}} onClick={handleClick}>יציאה</BiExit>
+            יציאה
+            </div>
+          
+          <div style={{cursor:'pointer',display:'flex',flexDirection:'column'}}>
+          <MdRemoveCircleOutline style={{width:'3em',height:'3em'}} onClick={handleRemove}>מחיקה</MdRemoveCircleOutline>
+          מחיקה
+          </div>
+      
           </div>
      
         </div>
 
        </Modal>
-       }
+       } 
 
     </div>
     </div>
