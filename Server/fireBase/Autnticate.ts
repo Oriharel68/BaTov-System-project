3f12880bf241a3b196cb4275cd0f82b4d3b9bb06
Oriharel68 <oriharel68@gmail.com>
2023-08-25 22:45:23 +0300

    fix

--- Server/fireBase/Autnticate.ts
+++ Server/fireBase/Autnticate.ts
@@ -6,7 +6,9 @@ import { Request, Response,NextFunction } from "express";
 
 const Verify = async (req:Request,res:Response,next:NextFunction) =>{
     const IdToken = req.cookies?.IdToken;
-    
+    if(!IdToken){
+        return res.status(401).send('empty');
+    }
     
     const Auth = getAuth();
     Auth.verifyIdToken(IdToken).then((decode)=>{
