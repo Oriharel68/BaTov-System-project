d3269e27c1a1418f264646777885d01c08f4528f
Oriharel68 <oriharel68@gmail.com>
2023-08-26 18:37:34 +0300

    fix same-Site option

--- Server/Router/login.ts
+++ Server/Router/login.ts
@@ -12,7 +12,7 @@ router.post('/',async(req:Request,res:Response)=>{
     const uid = req.body?.uid;
     if(!uid)return res.status(401).send();
     const token = jwt.sign(uid,process.env.ACCESS_TOKEN_SECRET || '');
-    res.cookie('accessToken',token,{httpOnly:true});
+    res.cookie('accessToken',token,{httpOnly:true ,sameSite:'none'});
     res.sendStatus(200);
 })
 
