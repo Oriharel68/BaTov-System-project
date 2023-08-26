3bd83ebd7b1adc30f0267362d2812f7635e26023
Oriharel68 <oriharel68@gmail.com>
2023-08-26 20:08:45 +0300

    fix

--- Server/Router/login.ts
+++ Server/Router/login.ts
@@ -12,12 +12,11 @@ const ClientsModel:Model<Client> = require('../models/ClientsModel');
 
 router.post('/',async(req:Request,res:Response)=>{
     const uid = req.body?.uid;
-    const isCompany = req.body.isCompany;
 
     if(!uid)return res.sendStatus(400);
     const client = await ClientsModel.findOne({firebaseUid:uid});
     if(!client)return res.sendStatus(400);
-    const token = jwt.sign({uid,isCompany},process.env.ACCESS_TOKEN_SECRET || '');
+    const token = jwt.sign(uid,process.env.ACCESS_TOKEN_SECRET || '');
     res.cookie('accessToken',token,{httpOnly:true ,sameSite:'none',secure:true});
     res.sendStatus(200);
 })
