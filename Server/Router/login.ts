940cce6b17021aafdbe55c788b758aa098dbc01b
Oriharel68 <oriharel68@gmail.com>
2023-08-26 20:16:20 +0300

    commit

--- Server/Router/login.ts
+++ Server/Router/login.ts
@@ -11,14 +11,18 @@ const jwt = require('jsonwebtoken');
 const ClientsModel:Model<Client> = require('../models/ClientsModel');
 
 router.post('/',async(req:Request,res:Response)=>{
-    const uid = req.body?.uid;
-
-    if(!uid)return res.sendStatus(400);
-    const client = await ClientsModel.findOne({firebaseUid:uid});
-    if(!client)return res.sendStatus(400);
-    const token = jwt.sign(uid,process.env.ACCESS_TOKEN_SECRET || '');
-    res.cookie('accessToken',token,{httpOnly:true ,sameSite:'none',secure:true});
-    return res.sendStatus(200);
+    try {
+        const uid = req.body?.uid;
+        if(!uid)return res.sendStatus(400);
+        const client = await ClientsModel.findOne({firebaseUid:uid});
+        if(!client)return res.sendStatus(400);
+        const token = jwt.sign(uid,process.env.ACCESS_TOKEN_SECRET || '');
+        res.cookie('accessToken',token,{httpOnly:true ,sameSite:'none',secure:true});
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+  
 })
 
 
