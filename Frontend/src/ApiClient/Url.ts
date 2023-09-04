e8cf70055abfc003b2225f1c725b52a6c86e2182
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-09-04 20:28:26 +0300

    changes fix

--- Frontend/src/ApiClient/Url.ts
+++ Frontend/src/ApiClient/Url.ts
@@ -1,8 +1,5 @@
-
-
 const Url = 'https://batov.onrender.com';
 const SecondServer = 'https://ba-tov-system-project.vercel.app';
 const local = 'http://localhost:4000';
 
-const localIphone = 'http://192.168.68.107:4000';
-export default localIphone;
+export default SecondServer;
